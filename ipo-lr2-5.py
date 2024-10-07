a76266238facb7e14245400489869978abd49ea0
Yan <tal531764@gmail.com>
2024-10-07 22:24:49 +0300

    Add files via upload

---
+++ ipo-lr2-5.py
@@ -0,0 +1,8 @@
+import math
+a = int(input("Введите длину a: "))
+b = int(input("Введите длину b: "))
+c = int(input("Введите длину c: "))
+d = int(input("Введите длину d: "))
+p = (a + b + c + d) / (2)
+s = ((a + b) / math.fabs(a - b)) * math.sqrt(p - a) * (p - b) * (p - a - c) * (p - a - d)
+print (s)
